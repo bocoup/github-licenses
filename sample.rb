ccd9b999dfe2c0290be6e2ad71a2972f5f41d342
Darius Kazemi <darius.kazemi@gmail.com>
2011-12-16 12:52:19 -0500

    sample.rb shows a bunch of game projects and owners

--- sample.rb
+++ sample.rb
@@ -1,11 +1,9 @@
 require "rubygems"
 require "github_api"
+require "octokit"
 
-@github = Github.new
-
-
-@github.git_data.tree 'peter-murach', 'github', 'c18647b75d72f19c1e0cc8af031e5d833b7f12ea', :recursive => true do |file|
-  if /README/.match file.path
-    puts file.path
-  end
+Octokit.search_repos("game").each do |result|
+  puts "Owner: #{result.owner}, Project: #{result.url}"
 end
+
+
