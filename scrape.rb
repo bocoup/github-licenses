c1b31163266390f3eecd782be1e74df626a2364a
Mike Pennisi <mike@mikepennisi.com>
2011-12-16 14:54:03 -0500

    Dirty dirty hack to get scraper to grab license files from the net

--- scrape.rb
+++ scrape.rb
@@ -1,64 +1,40 @@
 require "rubygems"
 require "github_api"
+require "json"
+require "base64"
+require "octokit"
 
+@data = Array.new
 @github = Github.new
-@licenseFilePattern = /licen[cs]e/
+@licenseFilePattern = /licen[cs]e|mit/i
 @outfile = File.open("out.txt", "w")
 
-def processTree( tree )
-	puts "Tree", tree, "\n\n"
-=begin	tree.each { |files|
-		files.each { |file|
-		puts file, file.class, "\n\n"
-		if file.class != "Array"# && file.type == "blob"
-			if @licenseFilePattern.match file.path
-				@outfile.syswrite "Has License file!\n"
-			end
-		end
-		}
-	}
-=end
+def getMaster( author, repo )
+	@githubb = Github.new
+	@repo = @githubb.get('/repos/' + author + '/' + repo).to_hash
+	@master_branch = @repo["master_branch"]
+	@githubb.get('/repos/' + author + '/' + repo + '/commits', { :sha => @master_branch } )[0]
 end
 
 
-@githubb = Github.new
-@githubb.repos.get_repo('annasob', 'popcorn-js')
-puts @githubb
-exit
-
-#annasob/popcorn-js
-@repo = Github::Repos.new :user => 'annasob', :repo => 'popcorn-js'
-puts @repo.inspect
-puts @repo.methods.sort.join(", ")
-puts @repo.description
-exit
-#puts @repos.methods.sort
-@repo.branches do |branch|
-  @outfile.syswrite "Branch '" + branch.name + "'\n"
-  @outfile.syswrite "\tLatest commit: " + branch.commit.sha + "\n"
-  #puts branch.commit.methods.join(", ")
-  @tree = @github.git_data.tree 'annasob', 'popcorn-js', branch.commit.sha, :recursive => true do |file|
-    #processTree( @tree )
-    @outfile.syswrite "\t" + file.path + "\n"
-  end
+def inspectRepo( author, name )
+	puts "Mike", @masterbranch = getMaster( author, name )
+	#puts branch.commit.methods.join(", ")
+	@tree = @github.git_data.tree author, name, @masterbranch["sha"], :recursive => true do |file|
+	  if @licenseFilePattern.match file.path
+	  
+	    @gh = Github.new
+	  	@blog = @gh.get('/repos/' +author+ '/'  +name+ '/git/blobs/' + file.sha)
+		@outfile.syswrite "\t" + file.path + "\n"
+		@licensefile = File.open( 'licenses/' + author + '_' + name + '_' + file.path.gsub('/', '__') , "w")
+		@licensefile.syswrite( Base64.decode64(@blog.content) )
+	  end
+	end
 end
 
-
-
-=begin
-def scanRepo( author, reponame, tree )
-  findings = {}
-  @github.git_data.tree author, reponame, tree, :recursive => true do |file|
-    if @licenseFilePattern.match file.path
-      puts file.path
-    end
-    if /README/.match file.path
-      puts file.path
-    end
-  end
+Octokit.search_repos('game',{:start_page => 2}).each do |result|
+  inspectRepo( result.owner, result.name )
 end
+inspectRepo('annasob', 'popcorn-js' )
 
-scanRepo( 'peter-murach', 'github', 'c18647b75d72f19c1e0cc8af031e5d833b7f12ea' )
-scanRepo( 'jquery', 'jquery', '' );
-=end
 
