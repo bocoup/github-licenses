cc09e8b14d3739141c6be9cd4fa6aedbc9123edd
Mike Pennisi <mike@mikepennisi.com>
2011-12-16 13:50:01 -0500

    First attempt to retrieve master branch name

--- scrape.rb
+++ scrape.rb
@@ -2,10 +2,63 @@ require "rubygems"
 require "github_api"
 
 @github = Github.new
+@licenseFilePattern = /licen[cs]e/
+@outfile = File.open("out.txt", "w")
+
+def processTree( tree )
+	puts "Tree", tree, "\n\n"
+=begin	tree.each { |files|
+		files.each { |file|
+		puts file, file.class, "\n\n"
+		if file.class != "Array"# && file.type == "blob"
+			if @licenseFilePattern.match file.path
+				@outfile.syswrite "Has License file!\n"
+			end
+		end
+		}
+	}
+=end
+end
 
 
-@github.git_data.tree 'peter-murach', 'github', 'c18647b75d72f19c1e0cc8af031e5d833b7f12ea', :recursive => true do |file|
-  if /README/.match file.path
-    puts file.path
+@githubb = Github.new
+@githubb.repos.get_repo('annasob', 'popcorn-js')
+puts @githubb
+exit
+
+#annasob/popcorn-js
+@repo = Github::Repos.new :user => 'annasob', :repo => 'popcorn-js'
+puts @repo.inspect
+puts @repo.methods.sort.join(", ")
+puts @repo.description
+exit
+#puts @repos.methods.sort
+@repo.branches do |branch|
+  @outfile.syswrite "Branch '" + branch.name + "'\n"
+  @outfile.syswrite "\tLatest commit: " + branch.commit.sha + "\n"
+  #puts branch.commit.methods.join(", ")
+  @tree = @github.git_data.tree 'annasob', 'popcorn-js', branch.commit.sha, :recursive => true do |file|
+    #processTree( @tree )
+    @outfile.syswrite "\t" + file.path + "\n"
   end
 end
+
+
+
+=begin
+def scanRepo( author, reponame, tree )
+  findings = {}
+  @github.git_data.tree author, reponame, tree, :recursive => true do |file|
+    if @licenseFilePattern.match file.path
+      puts file.path
+    end
+    if /README/.match file.path
+      puts file.path
+    end
+  end
+end
+
+scanRepo( 'peter-murach', 'github', 'c18647b75d72f19c1e0cc8af031e5d833b7f12ea' )
+scanRepo( 'jquery', 'jquery', '' );
+=end
+
