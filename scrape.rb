d75f846af148ac9a9940d90c709b6aa4aeff59ba
Mike Pennisi <mike@mikepennisi.com>
2011-12-19 08:02:20 -0500

    Wrap blob requests in 'begin/rescue' statements. Organize code a little.

--- scrape.rb
+++ scrape.rb
@@ -6,7 +6,10 @@ require "octokit"
 
 @data = Array.new
 @github = Github.new
-@licenseFilePattern = /licen[cs]e|mit/i
+@filenamePatterns = {
+	:license => /licen[cs]e|mit/i,
+	:readme => /readme/i
+}
 @outfile = File.open("out.csv", "w")
 
 def getMaster( author, repo )
@@ -16,36 +19,56 @@ def getMaster( author, repo )
 	@githubb.get('/repos/' + author + '/' + repo + '/commits', { :sha => @master_branch } )[0]
 end
 
-
 def inspectRepo( author, name )
 	puts "Mike", @masterbranch = getMaster( author, name )
 
 	@found = false
+	@outfile.syswrite author + "," + name + ","
 	@tree = @github.git_data.tree author, name, @masterbranch["sha"] do |file|
-	  if @licenseFilePattern.match file.path
+	  if @filenamePatterns[:license].match file.path
 	  
 	    @gh = Github.new
 	    
-	  	@blog = @gh.get('/repos/' +author+ '/'  +name+ '/git/blobs/' + file.sha)
+	    begin
+	  	  @blog = @gh.get('/repos/' +author+ '/'  +name+ '/git/blobs/' + file.sha)
+	  	rescue
+	  	  next
+	  	end
 	  	@license_file_name = author + '_' + name + '_' + file.path.gsub('/', '__')
 		@licensefile = File.open( 'licenses/' + @license_file_name , "w")
 		@licensefile.syswrite( Base64.decode64(@blog.content) )
-		puts file
-		@outfile.syswrite author + "," + name + ",https://raw.github.com/" + author + "/" + name + "/" +@masterbranch["sha"]+ "/" + file.path + "," + @license_file_name + "\n"
+		puts "\n\n\n", file, "\n\n\n", "PATH:" + file.path, "\n\n\n"
+		@outfile.syswrite "https://raw.github.com/" + author + "/" + name + "/" +@masterbranch["sha"]+ "/" + file.path + "," + @license_file_name + ",,\n"
 		@found = true
-	  end
+
+	  if @filenamePatterns[:readme].match file.path
+	  
+	    @gh = Github.new
+	    begin
+	  	  @blog = @gh.get('/repos/' +author+ '/'  +name+ '/git/blobs/' + file.sha)
+	  	rescue
+		  next
+		end
+	  	@readme_file_name = author + '_' + name + '_' + file.path.gsub('/', '__')
+		@licensefile = File.open( 'readmes/' + @readme_file_name , "w")
+		@licensefile.syswrite( Base64.decode64(@blog.content) )
+		puts "\n\n\n", file, "\n\n\n", "PATH:" + file.path, "\n\n\n"
+		@outfile.syswrite "https://raw.github.com/" + author + "/" + name + "/" +@masterbranch["sha"]+ "/" + file.path + ",,," + @readme_file_name + "\n"
+		@found = true
+      end
+
 	end
 	
 	if !@found
-	  @outfile.syswrite author + "," + name + ",n/a,n/a\n"
+	  @outfile.syswrite author + "," + name + ",,,,\n"
 	end
 end
 
-@outfile.syswrite "Author,Repo Name,License File URL,License File Name\n"
+@outfile.syswrite "Author,Repo Name,License File URL,License File Name,Readme File URL,Readme File Name\n"
 
 Octokit.search_repos('game',{:start_page => 2}).each do |result|
   inspectRepo( result.owner, result.name )
 end
-inspectRepo('annasob', 'popcorn-js' )
+#inspectRepo('annasob', 'popcorn-js' )
 
 
